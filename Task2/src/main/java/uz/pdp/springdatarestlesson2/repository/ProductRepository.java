caf9c2ccc7f55a50afcfa12960e2e213d4e51659
BekKucharov <bek.jr.kucharov@gmail.com>
2021-04-08 11:48:44 +0500

    FinalCommit

---
+++ Task2/src/main/java/uz/pdp/springdatarestlesson2/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package uz.pdp.springdatarestlesson2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uz.pdp.springdatarestlesson2.entity.Product;
+import uz.pdp.springdatarestlesson2.projection.ProductProjection;
+
+@RepositoryRestResource(path = "product", excerptProjection = ProductProjection.class)
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
